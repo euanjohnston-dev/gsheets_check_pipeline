439006d32197def50f83fc3f8e233ba54cb6a244
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-23 12:33:52 +0100

    added destination to config.toml

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import logging
 from gsheets_check import load_pipeline_with_ranges
 import base64
 from slack_messaging import pipeline_success, pipeline_failure
+import cProfile
 
-
-def gsheets_check_pipeline(data, context): 
+def gsheets_check_pipeline(data, context):  
     """Triggered by a Pub/Sub message."""
-    logging.info(f"Function triggered by Pub/Sub event: {data}")
+   # logging.info(f"Function triggered by Pub/Sub event: {data}")
 
     try:
         message = base64.b64decode(data['data']).decode('utf-8')
         logging.info(f"Received message data: {message}")
-       
+
+
         load_pipeline_with_ranges()
         pipeline_success()
+
 
         logging.info("DBT command executed successfully.")
     except Exception as e:
         pipeline_failure(e)
         logging.error(f"Error executing DBT command: {e}")
+
