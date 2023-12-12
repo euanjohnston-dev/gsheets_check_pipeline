7abff002bd8333cd6f3184b8f3b9fba6f9c7dc2f
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-12-12 11:49:49 +0100

    slack webhook added

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import logging
 from gsheets_check import load_pipeline_with_ranges
 import base64
-from slack_messaging import pipeline_success, pipeline_failure
 import cProfile
 from duplicate_group_attribution import run_dupe_attribution
+import os
+from dlt.common.runtime.slack import send_slack_message
+from dotenv import load_dotenv
 
+def notify_on_completion(hook):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                message = f"Function {func.__name__} completed successfully"
+                send_slack_message(hook, message)
+                return result
+            except Exception as e:
+                message = f"Function {func.__name__} failed. Error: {str(e)}"
+                send_slack_message(hook, message)
+                raise
+        return wrapper
+    return decorator
 
+load_dotenv() #  utilised to got local env variable 
+slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
+
+@notify_on_completion(slack_webhook_url)
 def gsheets_check_pipeline(data, context):  
     """Triggered by a Pub/Sub message."""
     logging.info(f"Function triggered by Pub/Sub event: {data}")
@@ -14,12 +34,9 @@ def gsheets_check_pipeline(data, context):
         message = base64.b64decode(data['data']).decode('utf-8')
         logging.info(f"Received message data: {message}")
 
-
         load_pipeline_with_ranges()
-        pipeline_success()
         run_dupe_attribution()
 
         logging.info("DBT command executed successfully.")
     except Exception as e:
-        pipeline_failure(e)
         logging.error(f"Error executing DBT command: {e}")
